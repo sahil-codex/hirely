fcd400a57e33d2437ecb04733dc4b28488bcdf2f
Sahil-codex <sahil2flamingo@example.com>
2026-03-24 18:45:02 +0530

    first commit1

---
+++ app/api/auth/signup/route.tsx
@@ -0,0 +1,28 @@
+import {sql} from '@/lib/db';
+import bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+
+export async function POST(req:Request){
+    try{
+        const{email,password,role} = await req.json();
+        
+        if(!email||!password||!role){
+            return NextResponse.json({error:'Missing fields'},{status:400});
+
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const result = await sql`
+         INSERT INTO users (email,password_hash,role)
+         VALUES (${email.toLowerCase()},${hashedPassword},${role})
+         RETURNING IdleDeadline,email,role)`;
+        
+         return NextResponse.json({user:result[0]});
+        } catch(err){
+            const message = err instanceof Error? err.message:'Something went wrong';
+            return NextResponse.json({error:message},{status:500});
+        }
+    
+}
