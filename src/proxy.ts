77ee796e2b400b3c9350831088bc890dbb28d556
Sahil-codex <sahil2flamingo@gmail.com>
2026-03-25 19:08:09 +0530

    created proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,39 +1,45 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import {jwtVerify} from "jose";
-// import path from "path";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import {jwtVerify} from "jose";
 
-// const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
-// async function verifyToken(token){
-//     try{
-//         const {payload} = await jwtVerify(token,secret);
-//         return payload;
-//     }catch(err){
-//         return null;
-//     }
-// }
-// export async function proxy(req:NextRequest){
-//     const {pathname} = req.nextUrl;
 
-//     if(pathname.startsWith("/api/auth")||pathname.startsWith("/_next")||pathname==="/"){
-//         return NextResponse.next();
-//     }
-//     const token = req.cookies.get("token")?.value;
+const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+async function verifyToken(token:string){
+    try{
+        const {payload} = await jwtVerify(token,secret);
+        return payload;
+    }catch(err){
+        return null;
+    }
+}
+export async function proxy(req:NextRequest){
+    const {pathname} = req.nextUrl;
 
-//     if(!token){
-//         return NextResponse.redirect(new URL("/login",req.url));
-//     }
+    if(pathname.startsWith("/api/auth")||pathname.startsWith("/_next")||pathname==="/"){
+        return NextResponse.next();
+    }
+    const authHeader = req.headers.get("authorization");
 
-//     try{
-//         const {payload} = await jwtVerify(token,secret);
-//         if(pathname.startsWith("/admin")&& payload.role !=="ADMIN"){
-//         return NextResponse.redirect(new URL("/",req.url));
-//     }
-//        return NextResponse.next();
-//    }catch{
-//         return NextResponse.redirect(new URL("/login",req.url));
-//     }
-// }
-//     export const config ={
-//         matcher:["/dashboard/:path*","/api/:path*"],
-//     };
+    if(!authHeader){
+        if (pathname.startsWith("/api")) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+        return NextResponse.redirect(new URL("/login",req.url));
+    }
+    const token = authHeader.split(" ")[1];
+    const user = (await verifyToken(token))as {role:string}|null;
+   
+    if(!user){
+        return NextResponse.json({error:'Invalid token'},{status:401});
+    }
+    if(pathname.startsWith('/api/jobs/create')){
+        if(user.role!=='RECRUITER'){
+            return NextResponse.json({error:'Forbidden'},{status:403});
+    }
+}
+ return NextResponse.next();
+}
+    export const config ={
+        matcher:["/dashboard/:path*","/api/:path*"],
+    };
+
