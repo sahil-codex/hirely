07d23d2401f521aef5646f5037eee7aebaac5064
Sahil-codex <sahil2flamingo@gmail.com>
2026-04-08 15:51:44 +0530

    styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="bg-background text-gray-800">
+        <div className="min-h-screen px-6 py-4">{children}
+          </div>
+        </body>
     </html>
   );
 }
