8715287514ba46905127fe59854fdf3e1bfbc2ff
Sahil-codex <sahil2flamingo@gmail.com>
2026-04-14 12:22:08 +0530

    update dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { desc } from "drizzle-orm";
 import { useState } from "react";
 
-export async function dashboardPage(){
+export function DashboardPage(){
     const [title,setTitle] = useState("");
     const [description,setDescription] = useState("");
     const [location,setLocation] = useState("");
@@ -20,8 +19,9 @@ export async function dashboardPage(){
             const res = await fetch("/api/jobs/create",{
                 method:"POST",
                 headers:{
-                    "Content-Type":"applicaton/json",
+                    "Content-Type":"application/json",
                 },
+                credentials: "include",
                 body:JSON.stringify({
                     title,
                     description,
@@ -50,14 +50,14 @@ export async function dashboardPage(){
     return (
         <div className="max-w-3xl mx-auto space-y-6">
             <h1 className="text-2xl font-semibold text-white">Recruiter Dashboard</h1>
-            <form onSubmit={handleCreateJob} className="bg-card border border-border rounded 2xl space-y-4">
+            <form onSubmit={handleCreateJob} className="bg-card border border-border rounded-2xl space-y-4 p-6">
                 {message &&(
                     <p className="text-sm text-center text-primary">{message}</p>
                 )}
                 <input placeholder="Job Title" className="input" value={title} onChange={(e)=>setTitle(e.target.value)} required />
-                <textarea placeholder="Job Description" value={description} onChange={(e)=>setDescription(e.target.value)} required />
+                <textarea className="input" placeholder="Job Description" value={description} onChange={(e)=>setDescription(e.target.value)} required />
                 <input placeholder="Location" className="input" value={location} onChange={(e)=>setLocation(e.target.value)} />
-                <input placeholder="Salary" type={salary} onChange={(e)=>setSalary(e.target.value)} />
+                <input placeholder="Salary" type="number" onChange={(e)=>setSalary(e.target.value)} />
                 <input placeholder="Skills (comma seperated)" className="input" value={skills} onChange={(e)=>setSkills(e.target.value)} />
                 <button type="submit" disabled ={loading} className="w-full bg-primary py-2 rounded-xl text-white hover:opacity-90 transition">{loading ? "Creating...":"Create Job"}</button>    
             
