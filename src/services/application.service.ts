4f01f032d0922c54aa25c7576b2401d9ef8fd230
Sahil-codex <sahil2flamingo@gmail.com>
2026-04-19 18:23:19 +0530

    made applyJobService

--- src/services/application.service.ts
+++ src/services/application.service.ts
@@ -1,6 +1,7 @@
 import{
     createApplication,checkExistingApplication,
 } from "@/repositories/application.repository";
+import {getApplicationsByJob} from "@/repositories/application.repository";
 
 export async function applyToJobService(user:any,jobId:string){
     if(user.role!=="CANDIDATE"){
@@ -16,4 +17,17 @@ export async function applyToJobService(user:any,jobId:string){
             throw new Error("You have already applied to this job");
         }
         return await createApplication(user.userId,jobId);
+}
+
+export async function getApplicationsForJobService(
+    user:{userId:string;role:string},
+    jobId:string
+){
+    if(user.role!=="RECRUITER"){
+        throw new Error("Only recruiters allowed");
+    }
+    if(!jobId){
+        throw new Error("Job ID required");
+    }
+    return await getApplicationsByJob(jobId);
 }
