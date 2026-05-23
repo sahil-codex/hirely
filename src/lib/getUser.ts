9bc2c313eae3bb87f5283e50ddfd0bf985855efc
Sahil-codex <sahil2flamingo@gmail.com>
2026-05-23 14:04:59 +0530

    update param

--- src/lib/getUser.ts
+++ src/lib/getUser.ts
@@ -1,8 +1,9 @@
 import { jwtVerify } from "jose";
 import {cookies} from "next/headers";
+import { NextRequest } from "next/server";
 const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
-export async function getUserFromRequest(){
+export async function getUserFromRequest(req:NextRequest){
     try{
     const cookieStore = await cookies();
     const token = cookieStore.get("token")?.value;
